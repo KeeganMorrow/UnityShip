f224c4113965da0d2155b2ce04d8ddd474e2f78d
Keegan Morrow <Korrow@gmail.com>
2017-06-12 17:19:55 -0700

    Add test highlighting on mouseover WIP

---
+++ Assets/Scripts/Effects/MouseHighlight.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHighlight : MonoBehaviour {
+    public Material highlightMaterial;
+    private int materialIndex;
+
+    private void Start()
+    {
+        var materials = GetComponent<Renderer>().materials;
+        materialIndex = materials.GetLength(0);
+        var newMaterials = new Material[materialIndex + 1];
+        for (var i = 0; i < materialIndex; i++){
+            newMaterials[i] = materials[i];
+        }
+        GetComponent<Renderer>().materials = newMaterials;
+    }
+
+    private void OnMouseEnter()
+    {
+        var materials = GetComponent<Renderer>().materials;
+        materials[materialIndex] = highlightMaterial;
+        GetComponent<Renderer>().materials = materials;
+    }
+
+    private void OnMouseExit()
+    {
+        var materials = GetComponent<Renderer>().materials;
+        materials[materialIndex] = null;
+        GetComponent<Renderer>().materials = materials;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
